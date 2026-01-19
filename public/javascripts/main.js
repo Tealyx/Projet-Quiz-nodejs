6e680f62cf7567db90ce848eb8637847988836a1
keniour <el.roussarie@gmail.com>
2026-01-19 16:10:40 +0100

    End of front-end

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,7 @@
 
 const btn_page_installation = document.getElementById("btn_page_installation");
 btn_page_installation.addEventListener("click",function(){
-    //A faire
+    console.log('appuie sur le bouton page installation')
 });
 
 
@@ -11,5 +11,19 @@ const btn_activation_structure_page=document.getElementById("btn_activation_stru
 btn_activation_structure_page.addEventListener("click",function(){ //J'ajoute à l'élément un mouchard qui écoute si quelqu'un "click" sur l'élément. Si le mouchard détecte un "click", alors la fonction "function" est éxecuté.
     console.log("Un click a été détecté sur le bouton : 'btn_activation_structure_page'")
     const Header = document.getElementById("Header")
-    Header.style.border="2px solid black"
+    if (Header.style.border === '2px solid black') { //Deux méthodes pour activer/Désactiver des éléments esthétiques
+    Header.style.border = 'none';  // Désactiver la bordure
+    Header.style.margin = "none";
+    } else {
+    Header.style.border = '2px solid black';  // Ou activer la bordure
+    Header.style.margin = "3px";
+    };
+    Header.classList.toggle('show-header-label');
+    const Main = document.getElementById("Main");
+    Main.classList.toggle('active_borders'); //Deuxième méthode rajouter ou enlever la classe 'active-borders' définie dans le CSS à la liste des classes de l'élément Main.
+    Main.classList.toggle('show-main-label')
+    const Footer = document.getElementById("Footer");
+    Footer.classList.toggle('active_borders')
+    Footer.classList.toggle('show-footer-label')
+    
 });
